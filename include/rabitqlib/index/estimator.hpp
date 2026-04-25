fa28bacf3910df863bb2cd4e207691cc1bd74c77
Tamim Ehsan <tamimehsan99@gmail.com>
2026-04-25 11:30:24 +0800

    Refactor query handling to support transposed query bins in SplitSingleQuery and warmup_ip_x0_q

--- include/rabitqlib/index/estimator.hpp
+++ include/rabitqlib/index/estimator.hpp
@@ -175,7 +175,7 @@ inline void split_single_estdist(
 
     ip_x0_qr = warmup_ip_x0_q<SplitSingleQuery<float>::kNumBits>(
         cur_bin.bin_code(),
-        q_obj.query_bin(),
+        q_obj.transposed_query_bin(),
         q_obj.delta(),
         q_obj.vl(),
         padded_dim,
